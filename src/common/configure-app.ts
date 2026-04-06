58ce2daeb7c510bf10883096c30bf7219c56c326
mr7aali <mr7aali@gmail.com>
2026-04-07 03:27:59 +0600

    added globalPrefix

--- src/common/configure-app.ts
+++ src/common/configure-app.ts
@@ -1,10 +1,31 @@
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { GlobalExceptionFilter } from './filters/global-exception.filter';
 import { ResponseInterceptor } from './interceptors/response.interceptor';
 
 export function configureApp(app: INestApplication): INestApplication {
+  const globalPrefix = process.env.GLOBAL_PREFIX ?? 'api';
+
+  app.setGlobalPrefix(globalPrefix);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
   app.useGlobalInterceptors(new ResponseInterceptor());
   app.useGlobalFilters(new GlobalExceptionFilter());
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Excel Assignment API')
+    .setDescription('API documentation for the Excel Assignment service')
+    .setVersion('1.0')
+    .addServer(`/${globalPrefix}`)
+    .build();
+
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup(`${globalPrefix}/docs`, app, swaggerDocument);
 
   return app;
 }
