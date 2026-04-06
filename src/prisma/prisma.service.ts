19fe6281136951a25a30622910d935c65842ed35
mr7aali <mr7aali@gmail.com>
2026-04-06 10:07:41 +0600

    prisma module  added

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,42 @@
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+// import { PrismaClient } from '../../generated/prisma/client.js';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from 'generated/prisma/client';
+// import { PrismaClient } from '../generated/prisma/client.js';
+// import { PrismaClient } from '@prisma/client/extension';
+// import { PrismaClient } from 'src/generated/prisma/client.js';
+// import { PrismaClient } from 'src/generated/prisma/client';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    const databaseUrl = process.env.DATABASE_URL;
+
+    if (!databaseUrl) {
+      throw new Error('DATABASE_URL is not defined');
+    }
+
+    const adapter = new PrismaPg({
+      connectionString: databaseUrl,
+    });
+
+    super({ adapter });
+  }
+
+  async onModuleInit() {
+    try {
+      await this.$connect();
+      console.log('✅ Prisma connected to database');
+    } catch (err) {
+      console.error('❌ Prisma connection failed', err);
+      throw err;
+    }
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+}
